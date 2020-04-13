4ae74c634a54a3c4e44e3147b2d03aa656096097
YangHaiBo <1392593558@qq.com>
2020-04-13 16:16:28 +0800

    create project

---
+++ src/test/java/com/yhb/studycommunity/StudyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhb.studycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
